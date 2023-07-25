10d441060e33df98cccbe743c942abcb53e85d82
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-25 21:37:36 +0300

    added routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { 
+         Routes, 
+         Route,
+         createBrowserRouter,
+         createRoutesFromElements
+        } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import AddItem from './pages/AddItem'
+import Cart from './pages/Cart'
+
 function App() {
  
 
   return (
     <>
-     <Header/>
+     <Routes>
+      <Route path='/' element={<Header/>}/>
+      <Route path='/addItem' element={<AddItem/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+     </Routes>
     </>
   )
 }
