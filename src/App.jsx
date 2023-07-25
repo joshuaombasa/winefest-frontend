ef7234f350de091025d19a8861e8c8b76f8448f4
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-25 23:25:27 +0300

    updated  routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
-import { 
-         Routes, 
-         Route,
-         createBrowserRouter,
-         createRoutesFromElements
-        } from 'react-router-dom'
+import {
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
 import './App.css'
-import Header from './components/Header'
+import MainLayout from './components/MainLayout'
 import AddItem from './pages/AddItem'
 import Cart from './pages/Cart'
+import Home from './pages/Home'
 
 function App() {
- 
+
 
   return (
     <>
-     <Routes>
-      <Route path='/' element={<Header/>}/>
-      <Route path='/addItem' element={<AddItem/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-     </Routes>
+      <Routes >
+        <Route path='/' element={<MainLayout />}>
+          <Route path='home' element={<Home/>}/>
+          <Route path='addItem' element={<AddItem />} />
+          <Route path='cart' element={<Cart />} />
+        </Route>
+      </Routes>
     </>
   )
 }
