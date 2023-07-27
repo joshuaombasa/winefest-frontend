bcb6ccc0dbb0d2b57761208a7fe05a97db122478
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-27 17:55:20 +0300

    updated Total component

---
+++ src/components/Total.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Total({ winesInCart }) {
+
+    let totalCost = 0
+
+    if (winesInCart) {
+        for (let i = 0 ; i < winesInCart.length ; i++) {
+            totalCost += winesInCart[i].price
+            
+        }
+    }
+
+   
+    
+
+    return (
+        <div className="total--wrapper">
+            <div className="total--cost--container">
+                <p className="total--cost--text">Total cost</p>
+                <p className="total--cost">$ { totalCost }</p>
+            </div>
+            <a  className="complete--order">PAY</a>
+        </div>
+    )
+}
