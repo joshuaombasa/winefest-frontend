7d1004b9e8172d9f2ca27dae3c16d6f8321bdba0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-05 10:34:32 +0300

    updated login logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,9 +17,27 @@ export default function Login() {
         ))
     }
 
+
     function handleSubmit(event) {
         event.preventDefault()
-        console.log(formData)
+    
+        const loginUser = async () => {
+            try {
+                const res = await fetch('http://localhost:3000/login', {
+                    method : 'POST',
+                    headers : {
+                        'Content-Type': 'application/json'
+                    },
+                    body : JSON.stringify(formData)
+                })
+                const data = await res.json()
+                console.log(data)
+            } catch(error) {
+                console.error(error)
+            }
+        }
+
+        loginUser()
     }
 
     return (
