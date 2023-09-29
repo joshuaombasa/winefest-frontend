9bb5afcd4aec96b545a075a4e24cd6bf7b346cc6
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-29 23:44:12 +0300

    updated logic to verify if user is logged in

--- src/pages/AddItem.jsx
+++ src/pages/AddItem.jsx
@@ -6,37 +6,35 @@ import Login from "./Login";
 export default function AddItem() {
 
     const [isLoggedIn, setIsLoggedIn] = React.useState(false)
-    const[authKey, setAuthKey] = React.useState(() => localStorage.getItem('token'))
+    // const[authKey, setAuthKey] = React.useState(() => localStorage.getItem('token'))
 
-    React.useEffect(() => {
-        fetch('http://localhost:3000/vendor', {
-            method: 'GET',
-            headers: {
-                'Authorization' : localStorage.getItem('token')
-            }
-        })
-        .then(res => {
-            if (res.ok) {
-                setIsLoggedIn(true)
-            } else {
-                return redirect("/login")
-            }
+    // React.useEffect(() => {
+    //     fetch('http://localhost:3000/vendor', {
+    //         method: 'GET',
+    //         headers: {
+    //             'Authorization' : localStorage.getItem('token')
+    //         }
+    //     })
+    //     .then(res => {
+    //         if (res.ok) {
+    //             setIsLoggedIn(true)
+    //         } else {
+    //             return redirect("/login")
+    //         }
 
-           return res.json()
-        })
-        .then(data => {
-            console.log(data)
+    //        return res.json()
+    //     })
+    //     .then(data => {
+    //         console.log(data)
 
-        })
-        .catch(error => {
-            console.error(error)
-        })
-    },[authKey])
+    //     })
+    //     .catch(error => {
+    //         console.error(error)
+    //     })
+    // },[authKey])
 
 
     return (
-        <>
-        {isLoggedIn ? <AddItemForm/> : <Login/>}
-        </>
+        <AddItemForm/>
     )
 }
